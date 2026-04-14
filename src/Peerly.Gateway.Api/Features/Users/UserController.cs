5683905ee474baec5bee6d6867bb1b156f00365b
Pavel Glebov <pglebov@ozon.ru>
2026-04-15 01:40:58 +0300

    Implement ConfirmEmail endpoint

--- src/Peerly.Gateway.Api/Features/Users/UserController.cs
+++ src/Peerly.Gateway.Api/Features/Users/UserController.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Peerly.Gateway.Api.Features.Users.ConfirmEmail;
 using Peerly.Gateway.Api.Features.Users.Register;
 using Peerly.Gateway.Api.Infrastructure;
 using Peerly.Gateway.Api.Infrastructure.Filters;
@@ -36,5 +37,22 @@ public sealed class UserController : ApplicationControllerBase
         var response = await _mediator.Send(query, cancellationToken);
 
         return MatchResult(response, success => Ok(new RegisterResponseBody { UserId = success.UserId }));
+    }
+
+    [AllowAnonymous]
+    [HttpGet("confirmEmail")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(ProblemDetails))]
+    public async Task<ActionResult<RegisterResponseBody>> ConfirmEmail(
+        [FromQuery] ConfirmEmailFilter filter,
+        CancellationToken cancellationToken)
+    {
+        var query = new ConfirmEmailCommand
+        {
+            Filter = filter
+        };
+        var response = await _mediator.Send(query, cancellationToken);
+
+        return MatchResult(response, Ok);
     }
 }
