53e764e2d1c3f126ff4954d09b0f98bff3c17bd3
Pavel Glebov <pglebov@ozon.ru>
2026-04-19 13:54:04 +0300

    Remove body for DeleteCourse endpoint

--- src/Peerly.Gateway.Api/Features/Courses/DeleteCourse/DeleteCourseProfile.cs
+++ src/Peerly.Gateway.Api/Features/Courses/DeleteCourse/DeleteCourseProfile.cs
@@ -8,9 +8,7 @@ public sealed class DeleteCourseProfile : Profile
 {
     public DeleteCourseProfile()
     {
-        CreateMap<DeleteCourseCommand, V1DeleteCourseRequest>()
-            .IncludeMembers(c => c.RequestBody);
-        CreateMap<DeleteCourseRequestBody, V1DeleteCourseRequest>();
+        CreateMap<DeleteCourseCommand, V1DeleteCourseRequest>();
         CreateMap<V1DeleteCourseResponse, Result<EmptyResponse>>();
         CreateMap<V1DeleteCourseResponse.Types.Success, EmptyResponse>(MemberList.Source);
     }
