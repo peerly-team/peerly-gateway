5ca0c47dee32ed716360d18d151f93830b152060
Pavel Glebov <pglebov@ozon.ru>
2026-04-20 21:34:58 +0300

    Implement DeleteSubmittedHomework endpoint

--- src/Peerly.Gateway.Api/Features/Submissions/SubmissionController.cs
+++ src/Peerly.Gateway.Api/Features/Submissions/SubmissionController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Peerly.Gateway.Api.Features.Homeworks.DeleteSubmittedHomework;
 using Peerly.Gateway.Api.Features.Homeworks.UpdateSubmittedHomework;
 using Peerly.Gateway.Api.Infrastructure;
 using Peerly.Gateway.Api.Infrastructure.Filters;
@@ -34,6 +35,24 @@ public sealed class SubmissionController : ApplicationControllerBase
             SubmittedHomeworkId = submissionId,
             StudentId = User.GetUserId(),
             RequestBody = requestBody
+        };
+        var response = await _mediator.Send(command, cancellationToken);
+
+        return response.Match(Ok, BadRequest, OtherError);
+    }
+
+    [HasPermission(ApiPermission.DeleteSubmittedHomework)]
+    [HttpDelete("{submissionId:long}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType(typeof(ProblemDetails))]
+    public async Task<ActionResult> DeleteSubmittedHomework(
+        [FromRoute] long submissionId,
+        CancellationToken cancellationToken)
+    {
+        var command = new DeleteSubmittedHomeworkCommand
+        {
+            SubmittedHomeworkId = submissionId,
+            StudentId = User.GetUserId()
         };
         var response = await _mediator.Send(command, cancellationToken);
 
