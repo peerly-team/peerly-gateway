8d288f11f519ca98689675f959c070e0742bbb2d
Pavel Glebov <pglebov@ozon.ru>
2026-05-10 18:12:57 +0300

    Change UserName to Name in RegisterRequestBody

--- src/Peerly.Gateway.Api/Features/Auth/Register/RegisterCommand.cs
+++ src/Peerly.Gateway.Api/Features/Auth/Register/RegisterCommand.cs
@@ -13,6 +13,6 @@ public sealed record RegisterRequestBody
 {
     public required string Email { get; init; }
     public required string Password { get; init; }
-    public required string UserName { get; init; }
+    public required string Name { get; init; }
     public required Role Role { get; init; }
 }
